2f71d3665ea7d328ae00556a151f6b1e04c03cc3
zbokhari <zbokhari@alumni.usc.edu>
2021-10-04 21:37:34 -0500

    update

--- src/components/Capabilities.js
+++ src/components/Capabilities.js
@@ -1,55 +1,39 @@
-import React from 'react'
-import { Switch, Route, Redirect } from 'react-router-dom';
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+
+import './Capabilities.css'
 
 
-// import './App.css'
-// import Navbar from "./Navbar"
-import SocialFollow from "./SocialFollow"
-// import logo from "./images/SSD-Logo.JPG"
 
-class Capabilities extends React.Component {
-    state = { advice: '' }
-
-    componentDidMount() {    
-        this.fetchAdvice()
-    }
-
-    fetchAdvice = () => {
-        axios.get('https://api.adviceslip.com/advice')
-        .then((response) => {
-            const {advice} = response.data.slip ;
-            this.setState({ advice: advice});
-        })
-        .catch((error) => {
-            console.log(error); 
-        });
-    }
-
-    render() {
-        const { advice } = this.state ;
-        return (
-            <div className="container">
-            <div className="app">
-              {/* <header class="site-header">
-              <h1>Silver Story Development LLC</h1>
-            <img className="logo" src={logo} alt="" />
-              </header> */}
-            <div className="card">
-              <h2>{advice}</h2>
-              <button className="buton" onClick={this.fetchAdvice}>
-                <span><h4>Unlock today's wisdom</h4></span>
-              </button>
-            </div>
+class Capabilities extends Component {
+  render() {
+    return (
+      <div className="capabilities-container">
+        <div className="capabilities-cards">
+          <div className="capabilities-card">
+            <h6>Front-end</h6>
+            <br />
+            <p>HTML 5 | XML | JavaScript | ES2020 | AJAX | jQuery 3.5.1 | Single Page Applications | React | React Native | Redux | Rest API | CSS 4 | Bootstrap 4.5 | Responsive Web Design</p>
           </div>
-          <footer>
-          <SocialFollow />
-          </footer>
+          <div className="capabilities-card">
+            <h6>Back-end</h6>
+            <br />
+            <p>C# | PHP | Python | Express | Node | Entity Framework | MongoDB | GraphQL | Microsoft SQL Server | MySQL| PostgreSQL | .NET 4.8 | ASP.NET:AJAX|MVC|Web Api</p>
+          </div>
+          <div className="capabilities-card">
+            <h6>Miscellaneous</h6>
+            <br />
+            <p>git | GitHub | GitHub Pages | Object-Orient Programming | WordPress | WooCommerce | Avada | Google Analytics | Google Tag Manager | Software Development Life Cycle | Agile Methodologies: Kanban | Scrum | JIRA | Confluence</p>
+          </div>
+          <div className="capabilities-card">
+            <h6>Amazon Web Services</h6>
+            <br />
+            <p>Management Console | CLI | SDKs | Infrastructure | Shared Responsibility Model | Suport Plans | Well-Architected Framework (Security, Reliability, Operational Excellence, Cost Optimization, Performance Efficiency) | Compute/Storage (EBS, EC2, ECR, EFS, S3, Elastic Beanstalk, Lambda) | Cost Management (Cist Explorer, Budgets, Cost and Usage Report, RI Reporting)  | Database (Aurora, DynamoDB, RDS, Redshift) | Management and Governance (Auto Scaling, CloudFormation, CloudTrail, CloudWatch, Config, Personal Health Dashboard, Service Catalog, Systems Manager, Trusted Advisor) | Network and COntent Delivery (CloudFront, Direct Connect, Elastic Load Balancing, Route 53, VPC</p>
           </div>
 
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Capabilities;
